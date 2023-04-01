fece1c648aea26cd7a682436d84e3e69654ef344
roy2pushkar <roy2000pushkar@gmail.com>
2023-04-01 18:59:41 +0530

    weather-app

--- index.js
+++ index.js
@@ -105,4 +105,12 @@ function renderWeatherInfo(weatherInfo) {
   console.log(weatherInfo);
 
   //fetch values from weatherINfo object and put it UI elements
+  cityName.innerText = weatherInfo?.name;
+  countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
+  desc.innerText = weatherInfo?.weather?.[0]?.description;
+  weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
+  temp.innerText = `${weatherInfo?.main?.temp} °C`;
+  windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+  humidity.innerText = `${weatherInfo?.main?.humidity}%`;
+  cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
 }
